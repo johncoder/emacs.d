d998bd66caf93a8b68fea0bff02444d0187e2f15
John Nelson <john.nelson@nbcuni.com>
2016-03-08 10:14:09 -0500

    C-S-A now sets mark to beginning of line

--- johncoder/keybindings.el
+++ johncoder/keybindings.el
@@ -1,13 +1,18 @@
-(delete-selection-mode 1)
-(global-set-key (kbd "C-,") 'imenu)
-
 (defun other-window-prev ()
   (interactive)
   (other-window -1))
 
-(global-set-key (kbd "C-<tab>") 'other-window)
-(global-set-key (kbd "C-S-<tab>") 'other-window-prev)
-
 (add-hook 'org-mode-hook
           '(lambda ()
              (define-key org-mode-map [(control tab)] nil)))
+
+(defun select-to-beginning-of-line ()
+  (interactive)
+  (set-mark-command nil)
+  (beginning-of-line))
+
+(delete-selection-mode 1)
+(global-set-key (kbd "C-,") 'imenu)
+(global-set-key (kbd "C-S-A") 'select-to-beginning-of-line)
+(global-set-key (kbd "C-<tab>") 'other-window)
+(global-set-key (kbd "C-S-<tab>") 'other-window-prev)
