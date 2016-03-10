399a03ac939a65c923e3e84249b9c4e5d9bda7d7
John Nelson <jnelson@johncoder.com>
2016-03-09 22:03:23 -0500

    Adds build.bat compilation for C common

--- johncoder/cpp.el
+++ johncoder/cpp.el
@@ -1,2 +1,18 @@
 (setq c-default-style "bsd")
 (setq-default c-basic-offset 4)
+
+;; COMPILATION
+
+(defun build-bat-compile-command ()
+  (file-at-git-root "build.bat"))
+
+(when (eq system-type 'windows-nt)
+  (add-hook 'c-mode-common-hook
+            (lambda ()
+              (set (make-local-variable 'compile-command)
+                   (build-bat-compile-command)))))
+
+;;(add-hook 'c++-mode-hook
+;;          (lambda ()
+;;            (set (make-local-variable 'compile-command)
+;;                 (build-bat-compile-command))))
