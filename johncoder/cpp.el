67ae3eb6829a88ca0831f1481293fe1083fadce1
John Nelson <john.nelson@nbcuni.com>
2016-03-11 13:14:57 -0500

    Adds OSX cpp builds

--- johncoder/cpp.el
+++ johncoder/cpp.el
@@ -6,13 +6,17 @@
 (defun build-bat-compile-command ()
   (file-at-git-root "build.bat"))
 
+(defun build-sh-compile-command ()
+  (file-at-git-root "build.sh"))
+
 (when (eq system-type 'windows-nt)
   (add-hook 'c-mode-common-hook
             (lambda ()
               (set (make-local-variable 'compile-command)
                    (build-bat-compile-command)))))
 
-;;(add-hook 'c++-mode-hook
-;;          (lambda ()
-;;            (set (make-local-variable 'compile-command)
-;;                 (build-bat-compile-command))))
+(when (eq system-type 'darwin)
+  (add-hook 'c-mode-common-hook
+            (lambda ()
+              (set (make-local-variable 'compile-command)
+                   (build-sh-compile-command)))))
