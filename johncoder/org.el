6cd27de8564bc7ff20eb42dba88d3d26d26473a9
John Nelson <john.nelson@nbcuni.com>
2016-03-15 18:07:25 -0400

    org-capture

--- johncoder/org.el
+++ johncoder/org.el
@@ -1,8 +1,34 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
+(define-key global-map "\C-cc" 'org-capture)
 
 (setq org-log-done t)
 
 (setq org-agenda-files (list "~/org/work.org"
                              "~/org/study.org"
                              "~/org/home.org"))
+
+
+;;;; --------------------------------------------------
+;;;; TEMPLATE PLACEHOLDERS
+;;;; --------------------------------------------------
+;;;; %U       Inactive timestamp
+;;;; %^{Name} Prompt for something
+;;;; %a       Annotation (org-store-link)
+;;;; %i       Active region
+;;;; %?       Cursor ending location
+
+(setq org-capture-templates
+      '(("c" "Clock item"
+         item (clock)
+         "  - %i%?")
+        ("g" "Thought"
+         entry (file+headline "~/org/thoughts.org")
+         "* Thought: %?\n")
+        ("f" "Follow Up"
+         entry (file+headline "~/org/todo.org")
+         "* TODO: %?\n  %i\n %a\n")
+        ("j" "Append journal entry"
+         entry (file+datetree "~/org/journal.org")
+	 "* %U %^{Title}\n%?")
+        ))
